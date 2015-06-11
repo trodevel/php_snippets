74c886196ecafd5a34e758dcb5dbc80b453799c5
trodevel <trodevel@gmail.com>
2015-06-12 01:46:41 +0200

    sync with svn

---
+++ easy_mysql.php
@@ -0,0 +1,62 @@
+<?php
+
+// $Revision: 1840 $ $Date:: 2015-06-11 #$ $Author: serge $
+
+function easy_mysql_connect__( $server, $username, $password, $database, & $connection, & $error )
+{
+    $connection = @mysql_connect( $server, $username, $password );
+
+    if( !$connection )
+    {
+        $error = "cannot connect to mysql";
+        return false;
+    }
+
+    $db = mysql_select_db( $database, $connection );
+
+    if( !$db )
+    {
+        mysql_close( $connection );
+        $error = "cannot open database '$database'";
+        return false;
+    }
+
+    return true;
+}
+
+function easy_mysql_fetch_assoc( $server, $username, $password, $database, $query, & $res, & $error )
+{
+    $connection = null;
+
+    if( !easy_mysql_connect__( $server, $username, $password, $database, $connection, $error ) )
+    {
+        return false;
+    }
+
+    $ses_sql    = mysql_query( $query, $connection );
+    $res        = mysql_fetch_assoc( $ses_sql );
+
+    mysql_close( $connection );
+
+    return true;
+}
+
+function easy_mysql_num_rows( $server, $username, $password, $database, $query, & $res, & $error )
+{
+    $connection = null;
+
+    if( !easy_mysql_connect__( $server, $username, $password, $database, $connection, $error ) )
+    {
+        return false;
+    }
+
+    $ses_sql    = mysql_query( $query, $connection );
+
+    $res        = mysql_num_rows( $ses_sql );
+
+    mysql_close( $connection );
+
+    return true;
+}
+
+?>
